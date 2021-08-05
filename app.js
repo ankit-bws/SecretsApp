70029c190492ff9f7956bea5dc5233f4642be875
Your Name <you@example.com>
2021-08-05 15:35:03 +0530

    Use md5 hashing instead of dotenv

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const { prototype } = require('events');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -48,7 +49,7 @@ app.get("/login", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password,
+        password: md5(req.body.password),
     });
     newUser.save(function(err){
         if(!err){
@@ -63,7 +64,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username; 
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err, foundUser){
         if(err){
             console.log(err);
